c532e0b6e668297e77417abcbe5f60e75351c7ce
Jaison5008 <jaison.eee@gmail.com>
2024-01-17 18:38:48 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router(); 
+
+var fs =require('fs'); 
+
+/* GET home page. */
+router.get("/", function (request, response) {
+  response.send("Node server timestamp");
+});
+
+//Creating date and time
+const time_stamp = new Date().toString();
+const date_time = new Date();
+const date = ("0" + date_time.getDate()).slice(-2);
+const month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+const year = date_time.getFullYear();
+const hours = date_time.getHours();
+const minutes = date_time.getMinutes();
+const seconds = date_time.getSeconds();
+const currentDateTime = date + "_" + month + "_" + year + "-" + hours + "_" + minutes + "_" + seconds;
+
+// adding new text file to a folder with content current timestamp; http://localhost:3000/file
+router.post("/file", function (request, response) {
+  fs.writeFile(`./textFile/${currentDateTime}.txt`, time_stamp, (err) => {
+      console.log("file created!!!");
+      response.send("file created!!!");
+  });
+});
+
+// retrieving the added text files list from folder   http://localhost:3000/file
+router.get("/file", function (request, response) {
+
+  fs.readdir("./textFile/", (err, file) => {
+      if (err) {
+          console.log("files not found" + err);
+          response.send("files not found" + err);
+      }
+      console.log("Available files:" + file);
+      response.send(file);
+  });
+})
+
+
+module.exports = router;
